a3ec569ed543d8317036e6c27a531fd42f88e6e8
onejulian <onejulian2014@gmail.com>
2023-01-12 21:46:12 -0500

    :sparkles: Adición de diseños adaptados a mobile y agregación de enlaces externos

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  showMenu = false;
+
+  toggleNavbar() {
+    this.showMenu = !this.showMenu;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth > 640) {
+      this.showMenu = false;
+    }
+  }
 
 }
